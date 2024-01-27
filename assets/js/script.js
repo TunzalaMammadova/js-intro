027284d05d25c4754ca7f0f2f11902dffd00fa24
Tünzalə <tunzale@Tuni-MacBook-Air.local>
2024-01-28 01:07:32 +0400

    added file

---
+++ assets/js/script.js
@@ -0,0 +1,125 @@
+"use strict"
+
+
+// var surname = "Huseynov";
+// var surname = "test";
+// console.log(surname);
+
+
+// let address = "Ehmedli"
+// console.log(address);
+
+
+// const age = 27;
+// console.log(age);
+
+// const name = "salam"
+// name = "test"
+
+
+// let surname = null;
+// console.log(surname);
+
+
+// isMarried = true;
+
+// isMarried = 100;
+
+// console.log(isMarried);
+
+// let price = 50;
+
+// price +=50;
+// console.log(price);
+
+
+// let stu = {
+//     name: "Semed",
+//     surname: "Huseynov",
+//     age:27
+// };
+
+// stu.name = "test"
+// console.log(stu.name + "-" + stu.surname);
+
+
+// let name = "XXX";
+// let surname = "YYY";
+
+// // let fullName = name + "-" + surname;
+
+// let fullName = '${name} ${surname}';
+
+// console.log(fullName)
+
+// let ages = [62,44,55,23,22, "salam"]
+// console.log(ages[ages.length-1])
+
+// let num1 = 100;
+// let num2 = 30;
+// let result = num1 + num2;
+
+// console.log(result)
+
+// let num3 = 5**3
+
+// console.log(num3)
+
+// let num1 = "salam"
+// let num2 = 55;
+
+// let result = num1 - num2;
+
+// console.log(result)
+
+
+
+// let num = 100;
+// num *= 2;
+// console.log(num)
+
+
+// let num1 = "55"
+// let num2 = 55;
+// console.log(num1===num2)
+
+
+// let number = 100;
+
+// if(number >90 && number<120){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+
+// let result = num == 100 ? "yes" : "no"
+
+// let weekDay = 3
+
+// switch (weekDay) {
+//     case 3:
+//         console.log("wednesday")
+//         break;
+//     case 1:
+//         console.log("monday")
+//     default:
+//         console.log("Not selected")
+//         break;
+// }
+
+// let nums = [4,4,75,34,30]
+
+//     if(nums[nums.length-1]===30){
+//         console.log(nums[nums.length-1])
+//     }else{
+//         console.log("not equal")
+//     }
+
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// let sum = 0;
+// for (let i = 0; i < nums.length; index++) {
+   
+//     sum += i;
+   
+// }
+// console.log(sum)
